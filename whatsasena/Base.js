92288f715984bf3f55549a028c6e41fd81f43f5a
Shefin! <104140006+5hefin@users.noreply.github.com>
2023-04-06 18:30:34 +0530

    Update Base.js

--- whatsasena/Base.js
+++ whatsasena/Base.js
@@ -1,16 +1,8 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-*/
-
-'use strict';
+"use strict";
 
 class Base {
     constructor(client) {
-        Object.defineProperty(this, 'client', { value: client });
+        Object.defineProperty(this, "client", { value: client });
     }
 
     _clone() {
